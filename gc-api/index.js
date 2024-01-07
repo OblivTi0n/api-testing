d716a321ff6be5aeb1aec2461989d9a1dc2e643c
OblivTi0n <138016235+OblivTi0n@users.noreply.github.com>
2024-01-07 15:18:02 +1100

    change to 8080

--- gc-api/index.js
+++ gc-api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any available port
+const port = 8080; // You can choose any available port
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
